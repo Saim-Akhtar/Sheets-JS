5be077eb7b93f339296f7dab2b90699119558e75
Saim Akhtar <saimakhtar98@gmail.com>
2019-08-17 22:33:14 +0500

    SheetJs files added

---
+++ Convert Excel File to HTML Table/script.js
@@ -0,0 +1,27 @@
+let reader = new FileReader()
+const getFile=document.getElementById('fileUpload')
+const table=document.getElementById('table')
+getFile.addEventListener('change',(e)=>{
+    const myFile=e.target
+    console.log(myFile.files[0])
+    
+    reader.readAsArrayBuffer(myFile.files[0])
+    
+    reader.onload=()=>{
+        const data=new Uint8Array(reader.result)
+        
+        const wb = XLSX.read(data,{type:'array'});
+        
+        
+        try{
+            const htmlstr = XLSX.write(wb,{sheet:"Sheet1", type:'binary',bookType:'html'});
+            table.innerHTML=htmlstr    
+        }
+        catch(error){
+            console.log("File Foramt Not Supported As we have the Error: "+ error.message)
+            table.innerHTML=`Error: ${error.message}`
+        }
+        
+        
+    }
+})
